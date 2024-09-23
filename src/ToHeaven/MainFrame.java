8995a622781a7fad9bd3f27e6754f054174b2fc8
phatch <ohaomma@gmail.com>
2024-09-23 15:38:14 +0700

    DOING button for going other page

--- src/ToHeaven/MainFrame.java
+++ src/ToHeaven/MainFrame.java
@@ -1,15 +1,19 @@
-/*
+/* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package ToHeaven;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.*;
 
 /**
  *
  * @author LENOVO
  */
 public class MainFrame extends javax.swing.JFrame {
-
+     private Color color;
     /**
      * Creates new form MainFrame
      */
@@ -25,15 +29,23 @@ public class MainFrame extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-        java.awt.GridBagConstraints gridBagConstraints;
 
         Header = new javax.swing.JPanel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        name = new javax.swing.JLabel();
+        nameApp = new javax.swing.JLabel();
+        aboutUsjp = new javax.swing.JPanel();
+        aboutUsbutton = new javax.swing.JLabel();
+        productjp = new javax.swing.JPanel();
+        productbutton = new javax.swing.JLabel();
+        packagejp = new javax.swing.JPanel();
+        packagebutton = new javax.swing.JLabel();
+        costomizejp = new javax.swing.JPanel();
+        customizebutton = new javax.swing.JLabel();
+        homejp = new javax.swing.JPanel();
+        homebutton = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
         Body = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -42,23 +54,151 @@ public class MainFrame extends javax.swing.JFrame {
         Header.setBackground(new java.awt.Color(247, 247, 246));
         Header.setPreferredSize(new java.awt.Dimension(1920, 80));
 
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel4.setText("About Us");
+        nameApp.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        nameApp.setText("TO HEAVEN");
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel3.setText("Product");
+        aboutUsjp.setName("about us page"); // NOI18N
+        aboutUsjp.setPreferredSize(new java.awt.Dimension(100, 80));
 
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel5.setText("Package");
+        aboutUsbutton.setFont(new java.awt.Font("MiTNThin", 1, 18)); // NOI18N
+        aboutUsbutton.setText("เกี่ยวกับเรา");
+        aboutUsbutton.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
-        jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel6.setText("Cuztomize");
+        javax.swing.GroupLayout aboutUsjpLayout = new javax.swing.GroupLayout(aboutUsjp);
+        aboutUsjp.setLayout(aboutUsjpLayout);
+        aboutUsjpLayout.setHorizontalGroup(
+            aboutUsjpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(aboutUsjpLayout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addComponent(aboutUsbutton)
+                .addContainerGap(16, Short.MAX_VALUE))
+        );
+        aboutUsjpLayout.setVerticalGroup(
+            aboutUsjpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, aboutUsjpLayout.createSequentialGroup()
+                .addContainerGap(22, Short.MAX_VALUE)
+                .addComponent(aboutUsbutton, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(19, 19, 19))
+        );
 
-        jLabel7.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel7.setText("Home");
+        productjp.setAlignmentX(0.0F);
+        productjp.setName("product page"); // NOI18N
 
-        name.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        name.setText("TO HEAVEN");
+        productbutton.setFont(new java.awt.Font("MiTNThin", 1, 18)); // NOI18N
+        productbutton.setText("ผลิตภัณฑ์");
+
+        javax.swing.GroupLayout productjpLayout = new javax.swing.GroupLayout(productjp);
+        productjp.setLayout(productjpLayout);
+        productjpLayout.setHorizontalGroup(
+            productjpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, productjpLayout.createSequentialGroup()
+                .addContainerGap(17, Short.MAX_VALUE)
+                .addComponent(productbutton)
+                .addGap(14, 14, 14))
+        );
+        productjpLayout.setVerticalGroup(
+            productjpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, productjpLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(productbutton)
+                .addGap(23, 23, 23))
+        );
+
+        packagejp.setName("package page"); // NOI18N
+
+        packagebutton.setFont(new java.awt.Font("MiTNThin", 1, 18)); // NOI18N
+        packagebutton.setText("แพ็คเกจ");
+
+        javax.swing.GroupLayout packagejpLayout = new javax.swing.GroupLayout(packagejp);
+        packagejp.setLayout(packagejpLayout);
+        packagejpLayout.setHorizontalGroup(
+            packagejpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, packagejpLayout.createSequentialGroup()
+                .addContainerGap(23, Short.MAX_VALUE)
+                .addComponent(packagebutton)
+                .addGap(21, 21, 21))
+        );
+        packagejpLayout.setVerticalGroup(
+            packagejpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, packagejpLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(packagebutton)
+                .addGap(24, 24, 24))
+        );
+
+        costomizejp.setName("customize page"); // NOI18N
+
+        customizebutton.setFont(new java.awt.Font("MiTNThin", 1, 18)); // NOI18N
+        customizebutton.setText("ปรับแต่ง");
+
+        javax.swing.GroupLayout costomizejpLayout = new javax.swing.GroupLayout(costomizejp);
+        costomizejp.setLayout(costomizejpLayout);
+        costomizejpLayout.setHorizontalGroup(
+            costomizejpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(costomizejpLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(customizebutton)
+                .addContainerGap(21, Short.MAX_VALUE))
+        );
+        costomizejpLayout.setVerticalGroup(
+            costomizejpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, costomizejpLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(customizebutton)
+                .addGap(24, 24, 24))
+        );
+
+        homejp.setName("homepage"); // NOI18N
+        homejp.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                homejpMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                homejpMouseExited(evt);
+            }
+        });
+
+        homebutton.setFont(new java.awt.Font("MiTNThin", 1, 18)); // NOI18N
+        homebutton.setText("หน้าแรก");
+
+        javax.swing.GroupLayout homejpLayout = new javax.swing.GroupLayout(homejp);
+        homejp.setLayout(homejpLayout);
+        homejpLayout.setHorizontalGroup(
+            homejpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(homejpLayout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(homebutton)
+                .addContainerGap(22, Short.MAX_VALUE))
+        );
+        homejpLayout.setVerticalGroup(
+            homejpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, homejpLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(homebutton)
+                .addGap(26, 26, 26))
+        );
+
+        jButton2.setFont(new java.awt.Font("MiTNThin", 1, 18)); // NOI18N
+        jButton2.setText("ปรับแต่ง");
+
+        jButton3.setFont(new java.awt.Font("MiTNThin", 1, 18)); // NOI18N
+        jButton3.setText("แพ็คเกจ");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setFont(new java.awt.Font("MiTNThin", 1, 18)); // NOI18N
+        jButton4.setText("ผลิตภัณฑ์");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jButton6.setFont(new java.awt.Font("MiTNThin", 1, 18)); // NOI18N
+        jButton6.setText("เกี่ยวกับเรา");
 
         javax.swing.GroupLayout HeaderLayout = new javax.swing.GroupLayout(Header);
         Header.setLayout(HeaderLayout);
@@ -66,33 +206,40 @@ public class MainFrame extends javax.swing.JFrame {
             HeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(HeaderLayout.createSequentialGroup()
                 .addGap(32, 32, 32)
-                .addComponent(name)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 1385, Short.MAX_VALUE)
-                .addComponent(jLabel7)
+                .addComponent(nameApp)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton6)
+                .addGap(35, 35, 35)
+                .addComponent(jButton4)
+                .addGap(30, 30, 30)
+                .addComponent(jButton3)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel6)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel5)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel3)
-                .addGap(27, 27, 27)
-                .addComponent(jLabel4)
-                .addGap(30, 30, 30))
+                .addComponent(jButton2)
+                .addGap(192, 192, 192)
+                .addComponent(homejp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(costomizejp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(packagejp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(productjp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(aboutUsjp, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         HeaderLayout.setVerticalGroup(
             HeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(HeaderLayout.createSequentialGroup()
-                .addGap(29, 29, 29)
-                .addGroup(HeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel6)
-                    .addComponent(jLabel7))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButton6, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(homejp, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(costomizejp, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(packagejp, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(productjp, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(aboutUsjp, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, HeaderLayout.createSequentialGroup()
-                .addContainerGap(42, Short.MAX_VALUE)
-                .addComponent(name)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(nameApp)
                 .addContainerGap())
         );
 
@@ -104,6 +251,31 @@ public class MainFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void homejpMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_homejpMouseEntered
+        // TODO add your handling code here:
+        JPanel check = (JPanel) evt.getSource();
+        if (check.getName() == homejp.getName()){
+            color = homejp.getBackground();
+            homejp.setBackground(Color.yellow);
+        }
+    }//GEN-LAST:event_homejpMouseEntered
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void homejpMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_homejpMouseExited
+        // TODO add your handling code here:
+        JPanel check = (JPanel) evt.getSource();
+        if (check.getName() == homejp.getName()){
+            homejp.setBackground(color);
+        }
+    }//GEN-LAST:event_homejpMouseExited
 
     /**
      * @param args the command line arguments
@@ -143,11 +315,20 @@ public class MainFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Body;
     private javax.swing.JPanel Header;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel name;
+    private javax.swing.JLabel aboutUsbutton;
+    private javax.swing.JPanel aboutUsjp;
+    private javax.swing.JPanel costomizejp;
+    private javax.swing.JLabel customizebutton;
+    private javax.swing.JLabel homebutton;
+    private javax.swing.JPanel homejp;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JLabel nameApp;
+    private javax.swing.JLabel packagebutton;
+    private javax.swing.JPanel packagejp;
+    private javax.swing.JLabel productbutton;
+    private javax.swing.JPanel productjp;
     // End of variables declaration//GEN-END:variables
 }
