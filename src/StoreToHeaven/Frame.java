3e9733a78dcb42832c054556deaa001d1e5d150d
Trakanta4556 <trakanta.wo@ku.th>
2024-10-10 18:07:19 +0700

    update product

---
+++ src/StoreToHeaven/Frame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package StoreToHeaven;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Frame extends Product{
+     private String meterial;
+     public Frame(){
+     super();
+}
+}
--- src/ToHeaven/Product.java
+++ src/ToHeaven/Product.java
@@ -33,8 +33,11 @@ public class Product extends javax.swing.JPanel {
         price = new javax.swing.JLabel();
 
         setPreferredSize(new java.awt.Dimension(600, 220));
+        setLayout(null);
 
         productImage.setPreferredSize(new java.awt.Dimension(180, 180));
+        add(productImage);
+        productImage.setBounds(23, 25, 180, 180);
 
         detailProduct.setEditable(false);
         detailProduct.setColumns(20);
@@ -43,39 +46,14 @@ public class Product extends javax.swing.JPanel {
         detailProduct.setPreferredSize(new java.awt.Dimension(330, 140));
         detailOfProduct.setViewportView(detailProduct);
 
-        piece.setModel(new javax.swing.SpinnerNumberModel(0, 0, null, 1));
+        add(detailOfProduct);
+        detailOfProduct.setBounds(244, 25, 332, 144);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addComponent(productImage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(41, 41, 41)
-                        .addComponent(detailOfProduct, javax.swing.GroupLayout.PREFERRED_SIZE, 332, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(113, 113, 113)
-                        .addComponent(price, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(piece, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(69, 69, 69))))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(25, 25, 25)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(productImage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(detailOfProduct, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(piece, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(price, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-        );
+        piece.setModel(new javax.swing.SpinnerNumberModel(0, 0, null, 1));
+        add(piece);
+        piece.setBounds(443, 187, 64, 22);
+        add(price);
+        price.setBounds(316, 187, 80, 20);
     }// </editor-fold>//GEN-END:initComponents
 
 
