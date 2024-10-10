5f8f0c4fe741da1e0370a3abfc0f049ca6d29976
Trakanta4556 <trakanta.wo@ku.th>
2024-10-10 18:07:08 +0700

    update product

---
+++ src/DAO/Framedao.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import StoreToHeaven.AddFrame;
+import StoreToHeaven.FrameDetail;
+import java.awt.Component;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Framedao {
+    private Workbook wb;
+    private Sheet sheet;
+    private final String FILE_NAME = "StoreStock.xlsx";
+    private FileInputStream fileInput;
+    private FileOutputStream fos;
+    private ArrayList<List<String>> framelist = new ArrayList<>();
+    private String[] nameCol = { "ชื่อ","รายละเอียด" ,"pathรูปภาพ", "วัสดุ",  "ราคา","สี"};
+    private ArrayList<FrameDetail> fd;
+    private Component[] cmp;
+    
+    
+    public void save(AddFrame frame){
+         cmp = frame.getPic_detailJP().getComponents();
+         fd = new ArrayList<>();
+          for (Component c : cmp){
+            fd.add((FrameDetail) c);
+        }
+          read();
+          try {           // check that StoreStock.xlsx was found
+            if (sheet == null) {
+                System.out.println("Sheet not found");
+                return;
+            }
+            Row firstRow = sheet.getRow(0);
+            if (firstRow == null) {       // this files valid?
+                firstRow = sheet.createRow(0);          // create first row
+                nameCol[0] = wreath.getNameTF().getText();
+                /*bring all data in wreathlist to create each col in valid sheet*/
+                for(int j=0; j<nameCol.length; j++){
+                    Cell cell = firstRow.createCell(j);
+                    cell.setCellValue(nameCol[j]);
+                }
+            }
+            for (int a=0; a<fd.size(); a++){
+            String pattern = fd.get(a).getPatternTF().getText();
+            String detail = fd.get(a).getDetailTA().getText();
+            String path = fd.get(a).getFilePath();
+            String[] material = frame.getMaterialTF().getText().split(",");
+            String[] price = frame.getPriceTF().getText().split(",");
+            String[] dataChecked = {pattern,detail,path,String.join(",", material),String.join(",", price),String.join(",", color)};
+            boolean haveData = false ;
+            for (Row row : sheet){
+                Cell c = row.getCell(0);
+                if(c.toString().equals(dataChecked[0])){
+                    haveData = true;
+                    break;
+                }
+            }
+            if(!haveData){
+               try {
+                int lastRow = sheet.getLastRowNum();
+                Row newRow = sheet.createRow(lastRow+1);
+                newRow.createCell(0).setCellValue(pattern);
+                newRow.createCell(1).setCellValue(detail);
+                newRow.createCell(2).setCellValue(path);
+                newRow.createCell(3).setCellValue(frame.getMaterialTF().getText());
+                newRow.createCell(4).setCellValue(frame.getPriceTF().getText());
+               } catch (Exception e) {
+                    e.printStackTrace();
+               }             
+           }
+        }
+            /*write data into StoreStock.xlsx*/
+            fos = new FileOutputStream(new File(FILE_NAME));
+            wb.write(fos);
+        } catch (Exception e) {
+            e.printStackTrace(); // แสดงข้อผิดพลาด
+        } finally {
+            // ปิด resource ที่เปิดไว้
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+                if (wb != null) {
+                    wb.close();
+                }
+                if (fileInput != null) {
+                    fileInput.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void read() {
+        wb = null;
+        try {
+            fileInput = new FileInputStream(new File(FILE_NAME));
+            wb = new XSSFWorkbook(fileInput);
+            sheet = wb.getSheetAt(1); // เปลี่ยนไปที่ชีตแรก
+        } catch (Exception err) {
+            System.out.println("can't read file: " + err);
+        }
+    
+    }
+}
